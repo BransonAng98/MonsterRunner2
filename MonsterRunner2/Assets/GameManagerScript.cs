49b9b080d7adc99f81d1d928404244481e6f35a5
habbydelvin <delvinfoo@habby.com>
2024-02-29 10:18:56 +0800

    grid count test

--- MonsterRunner2/Assets/GameManagerScript.cs
+++ MonsterRunner2/Assets/GameManagerScript.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     public BossEnemyScript boss;
+    [SerializeField] private int gridCount;
+    [SerializeField] private List<GameObject> gridObjects = new List<GameObject>();
+
 
     void Start()
     {
         boss = GameObject.FindGameObjectWithTag("Monster").GetComponent<BossEnemyScript>();
         boss.gameObject.SetActive(false);
+        GameObject[] gridArray = GameObject.FindGameObjectsWithTag("Grid");
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (gridCount > 3)
+        {
+            GameObject objectToRemove = gridObjects[0]; // Remove the first object for simplicity, you can change this logic as per your requirement
+            gridObjects.Remove(objectToRemove);
+            Destroy(objectToRemove);
+            gridCount--;
+        }
     }
     void TurnMonsterOn()
     {
