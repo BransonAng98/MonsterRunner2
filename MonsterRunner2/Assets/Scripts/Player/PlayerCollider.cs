7bcb2723ad064f340096bbdbda3892a8c8846042
KeithHabby <keithwong@habby.com>
2024-03-11 16:33:35 +0800

    Mini Update
    Added more to the collision logic

--- MonsterRunner2/Assets/Scripts/Player/PlayerCollider.cs
+++ MonsterRunner2/Assets/Scripts/Player/PlayerCollider.cs
@@ -6,19 +6,39 @@ public class PlayerCollider : MonoBehaviour
 {
     public LayerMask enemyLayer;
     public PlayerSO playerData;
+    public List<Transform> enemyList = new List<Transform>();
 
     [SerializeField] float damage;
+    [SerializeField] DemoPlayer player;
 
     private void Start()
     {
         damage = playerData.damage;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<DemoPlayer>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == enemyLayer)
         {
-            //Damage enemy logic here, use the damage float
+            if (!enemyList.Contains(other.transform))
+            {
+                enemyList.Add(other.transform);
+                //Damage enemy logic here, use the damage float
+                player.TakeDamage(damage);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == enemyLayer)
+        {
+            if (enemyList.Contains(other.transform))
+            {
+                //Removes the enemy from the player's list
+                enemyList.Remove(other.transform);
+            }
         }
     }
 }
