73b050b09226661b34307bc4716dc0edcbbfff91
KeithHabby <keithwong@habby.com>
2024-03-14 12:04:41 +0800

    Mini Update
    Eliminated the floatiness when reverting back to straight driving issue

--- MonsterRunner2/Assets/Scripts/Player/DemoPlayer.cs
+++ MonsterRunner2/Assets/Scripts/Player/DemoPlayer.cs
@@ -263,7 +263,7 @@ public class DemoPlayer : MonoBehaviour
         {
             foreach(var wheel in wheels)
             {
-                wheel.wheelColliderl.steerAngle = Mathf.Lerp(wheel.wheelColliderl.steerAngle, 0f, 3f);
+                wheel.wheelColliderl.steerAngle = Mathf.Lerp(wheel.wheelColliderl.steerAngle, 0f, 0.3f);
             }
         }
     }
