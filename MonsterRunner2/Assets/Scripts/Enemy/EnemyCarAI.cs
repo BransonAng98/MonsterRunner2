91363f1746870c324c6a8dfab02ce1adc9cb4f88
habbydelvin <delvinfoo@habby.com>
2024-06-05 17:58:46 +0800

    Enemy Car AI Test

---
+++ MonsterRunner2/Assets/Scripts/Enemy/EnemyCarAI.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCarAI : MonoBehaviour
+{
+
+    [SerializeField] private Transform targetPositionTranform;
+
+    public enemyCarDriver carDriver;
+    private Vector3 targetPosition;
+
+    private void Awake()
+    {
+       
+    }
+
+    private void Update()
+    {
+        SetTargetPosition(targetPositionTranform.position);
+
+        float forwardAmount = 0f;
+        float turnAmount = 0f;
+
+        float reachedTargetDistance = 1f;
+        float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
+        if (distanceToTarget > reachedTargetDistance)
+        {
+            // Still too far, keep going
+            Vector3 dirToMovePosition = (targetPosition - transform.position).normalized;
+            float dot = Vector3.Dot(transform.forward, dirToMovePosition);
+
+            if (dot > 0)
+            {
+                // Target in front
+                forwardAmount = 1f;
+
+                float stoppingDistance = 30f;
+                float stoppingSpeed = 40f;
+                if (distanceToTarget < stoppingDistance && carDriver.GetSpeed() > stoppingSpeed)
+                {
+                    // Within stopping distance and moving forward too fast
+                    //forwardAmount = -1f;
+                }
+            }
+            else
+            {
+               
+            }
+
+            float angleToDir = Vector3.SignedAngle(transform.forward, dirToMovePosition, Vector3.up);
+            if (angleToDir == 0 || (angleToDir < 10 && angleToDir > -10))
+            {
+                turnAmount = 0f; // Don't steer if angle is within -10 to 10 degrees or exactly 0 degrees
+            }
+            else
+            {
+                if (angleToDir > 0)
+                {
+                    turnAmount = 1f;
+                }
+                else
+                {
+                    turnAmount = -1f;
+                }
+            }
+        }
+      else
+      {
+          // Reached target
+          if (carDriver.GetSpeed() > 15f)
+          {
+              forwardAmount = -1f;
+          }
+          else
+          {
+              forwardAmount = 0f;
+          }
+          turnAmount = 0f;
+      }
+
+        carDriver.SetInputs(forwardAmount, turnAmount);
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+
+}
