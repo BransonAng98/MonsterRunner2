10d8259a565a7567240ad10f894fd9b5973cda9d
BransonAng98 <bransonang@habby.com>
2024-06-27 13:41:11 +0800

    Update
    UI canvas update

--- MonsterRunner2/Assets/Scripts/GameManager/ScoreManagerScript.cs
+++ MonsterRunner2/Assets/Scripts/GameManager/ScoreManagerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ScoreManagerScript : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class ScoreManagerScript : MonoBehaviour
 
     public int currentEnemiesKilled;
     public int totalEnemiesKilled;
+
     public TextMeshProUGUI activeCounter;
     public TextMeshProUGUI goldamtCounter;
     public TextMeshProUGUI sideobjectiveText;
@@ -19,11 +21,16 @@ public class ScoreManagerScript : MonoBehaviour
     public TextMeshProUGUI questReward;
     public TextMeshProUGUI sideobjectiveDetail;
     public TextMeshProUGUI sideobjectiveReward;
+
     public QuestGiver questgiverScript;
-    public float distancetoTarget;
     public SideObjectiveQuestGiver SideObjectiveQuestGiverScript;
     public EnemySpawner enemySpawnerScript;
 
+    public GameObject timeIcon;
+    public GameObject killIcon;
+
+    private bool killQuest;
+    public float distancetoTarget;
     private int lastMissionCount = 0;
 
     void Start()
@@ -40,6 +47,7 @@ public class ScoreManagerScript : MonoBehaviour
         goldamtCounter.text = goldEarned + "";
         AssignObjectiveText();
         AssignSideObjectiveText();
+        UpdateObjIcon();
         // Check if missions completed has increased
         if (missionsCompleted > lastMissionCount)
         {
@@ -52,23 +60,40 @@ public class ScoreManagerScript : MonoBehaviour
 
             lastMissionCount = missionsCompleted;
         }
+
+
+    }
+
+    void UpdateObjIcon()
+    {
+        if (killQuest)
+        {
+            killIcon.SetActive(true);
+            timeIcon.SetActive(false);
+        }
+        else
+        {
+            killIcon.SetActive(false);
+            timeIcon.SetActive(true);
+        }
     }
 
     public void AssignObjectiveText()
     {
-        switch(questgiverScript.quest.goal.goaltype)
+        switch (questgiverScript.quest.goal.goaltype)
         {
             case "Kill":
                 questDetail.text = "Take out " + totalEnemiesKilled + " enemies and keep the cops distracted for us";
                 currentEnemiesKilled = questgiverScript.currentenemykilled;
                 totalEnemiesKilled = questgiverScript.enemykilled;
-                activeCounter.text = currentEnemiesKilled + "/" + totalEnemiesKilled  + " Killed";
+                activeCounter.text = currentEnemiesKilled + "/" + totalEnemiesKilled + "/" + "Killed";
                 questReward.text = "Reward: " + questgiverScript.quest.goldReward + "";
+                killQuest = true;
                 break;
             case "Reach":
                 distancetoTarget = questgiverScript.distancetoDestination;
                 int distanceInt = Mathf.FloorToInt(distancetoTarget);
-                questDetail.text = "Take us to the safe house. It's  " + distancetoTarget + " m away. Hurry!"; 
+                questDetail.text = "Take us to the safe house. It's  " + distancetoTarget + " m away. Hurry!";
                 activeCounter.text = distanceInt + "m";
                 questReward.text = "Reward: " + questgiverScript.quest.goldReward + "";
                 break;
@@ -86,27 +111,28 @@ public class ScoreManagerScript : MonoBehaviour
                 activeCounter.text = timeHolder;
                 questDetail.text = "Keep the cops busy for " + timeHolder + ". While you do that, we will rob the bank";
                 questReward.text = "Reward: " + questgiverScript.quest.goldReward + "";
+                killQuest = false;
                 break;
             case "None":
                 activeCounter.text = "Find a Quest!";
                 break;
-           
+
         }
     }
 
     public void AssignSideObjectiveText()
     {
-        switch(SideObjectiveQuestGiverScript.SideObjective.goal.objectiveType)
+        switch (SideObjectiveQuestGiverScript.SideObjective.goal.objectiveType)
         {
             case SideObjectiveGoal.ObjectiveType.Kill:
-            sideobjectiveDetail.text = "The boss says he will give you a bonus if you destroy " + SideObjectiveQuestGiverScript.enemykilled + " of their cars.";
-            sideobjectiveReward.text = SideObjectiveQuestGiverScript.SideObjective.goal.goldtobeEarnedAmt + "";
-            break;
+                sideobjectiveDetail.text = "The boss says he will give you a bonus if you destroy " + SideObjectiveQuestGiverScript.enemykilled + " of their cars.";
+                sideobjectiveReward.text = SideObjectiveQuestGiverScript.SideObjective.goal.goldtobeEarnedAmt + "";
+                break;
 
             case SideObjectiveGoal.ObjectiveType.EarnGold:
-            sideobjectiveDetail.text = "The boss is in a generous mood. If you earn " + SideObjectiveQuestGiverScript.SideObjective.goal.goldtobeEarnedAmt + " before you die, we'll give you a bonus";
-            sideobjectiveReward.text = "Reward: " + SideObjectiveQuestGiverScript.SideObjective.goldReward + "";
-            break;
+                sideobjectiveDetail.text = "The boss is in a generous mood. If you earn " + SideObjectiveQuestGiverScript.SideObjective.goal.goldtobeEarnedAmt + " before you die, we'll give you a bonus";
+                sideobjectiveReward.text = "Reward: " + SideObjectiveQuestGiverScript.SideObjective.goldReward + "";
+                break;
 
         }
     }
