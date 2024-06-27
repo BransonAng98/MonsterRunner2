030eea596c9fdf68df7f40a8b6d067ecc36e7df9
habbydelvin <delvinfoo@habby.com>
2024-06-27 14:38:49 +0800

    new tutorial enemy + score manager bug fixes

--- MonsterRunner2/Assets/Scripts/GameManager/ScoreManagerScript.cs
+++ MonsterRunner2/Assets/Scripts/GameManager/ScoreManagerScript.cs
@@ -86,7 +86,7 @@ public class ScoreManagerScript : MonoBehaviour
                 questDetail.text = "Take out " + totalEnemiesKilled + " enemies and keep the cops distracted for us";
                 currentEnemiesKilled = questgiverScript.currentenemykilled;
                 totalEnemiesKilled = questgiverScript.enemykilled;
-                activeCounter.text = currentEnemiesKilled + "/" + totalEnemiesKilled + "/" + "Killed";
+                activeCounter.text = currentEnemiesKilled + "/" + totalEnemiesKilled + " Killed";
                 questReward.text = "Reward: " + questgiverScript.quest.goldReward + "";
                 killQuest = true;
                 break;
