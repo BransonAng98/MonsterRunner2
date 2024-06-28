f830b39ed109b4fd02efedf9805b731bbf7a7c7b
KeithHabby <keithwong@habby.com>
2024-06-28 17:55:35 +0800

    Mini Update
    Fixed money display issue
    Fixed wrong button being shown on the result screen in the tutorial

--- MonsterRunner2/Assets/Scripts/GameManager/TutorialManager.cs
+++ MonsterRunner2/Assets/Scripts/GameManager/TutorialManager.cs
@@ -12,7 +12,8 @@ public class TutorialManager : MonoBehaviour
     public CanvasGroup fadeToBlackImage; 
     public float fadeDuration = 1f; // Duration of the fade effect
     public PlayerDataSO playerData;
-    public TextMeshProUGUI text;
+    public GameObject exitButton;
+    public GameObject retryButton;
 
     private bool shouldFadeIn = true;
     private float fadeTimer = 0f;
@@ -20,7 +21,7 @@ public class TutorialManager : MonoBehaviour
     private void Start()
     {
         fadeToBlackImage.alpha = 0f;
-        
+        exitButton.SetActive(false);
     }
 
     void StartFade()
@@ -37,7 +38,8 @@ public class TutorialManager : MonoBehaviour
 
     void StopPlayerMovement()
     {
-        text.text = "EXIT";
+        exitButton.SetActive(true);
+        retryButton.SetActive(false);
         playerManager.playerData.TakeDamage(1000);
         Invoke("DisplayEndScreen", 3.2f);
     }
